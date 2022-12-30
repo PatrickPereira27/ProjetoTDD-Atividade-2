fda049a926368563e8a4ddc7464436e5a41f466d
Patrick Pereira <patrick.pereira27@hotmail.com>
2022-12-30 14:43:05 -0300

    realização atividade modulo 2

---
+++ bds02-main/src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,52 @@
+package com.devsuperior.bds02.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.reporitories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class CityService {
+
+	@Autowired
+	private CityRepository repository;
+	
+	
+	public List<CityDTO> findAll(){
+		List<City> lista = repository.findAll(Sort.by("name"));
+		return lista.stream().map(c -> new CityDTO(c)).collect(Collectors.toList());
+	}
+
+
+	public CityDTO save(CityDTO dto) {
+		City city = new City();
+		city.setName(dto.getName());
+		city = repository.save(city);
+		return new CityDTO(city);
+		
+	}
+
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(String.format("Id %s não localizado para remoção", id));
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(String.format("Cidade com id %s não pode ser removida, vinculada a eventos", id));
+		}		
+	}
+	
+	
+	
+}
---
+++ bds02-main/src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,42 @@
+package com.devsuperior.bds02.services;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.reporitories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository repository;
+	
+	public EventDTO update(EventDTO dto, Long id) {
+		try {
+			//getOne não consulta o banco, ele simplesmente instancia um Produto com o id, bom porque não consulta o banco de dados duas vezes, evita de consultar e depois atualizar, ele só atualiza nesse caso
+			Event event =  repository.getOne(id); 
+			copyDtoToModel(dto,event);
+			
+			event = repository.save(event);
+			return new EventDTO(event);			
+		}catch (Exception e) {
+			throw new ResourceNotFoundException(String.format("Id %s não localizado para atualização", id));
+		}
+
+	}
+	
+	//método auxiliar
+	public void copyDtoToModel(EventDTO dto, Event event) {
+		event.setName(dto.getName());
+		event.setDate(dto.getDate());
+		event.setUrl(dto.getUrl());
+		event.setCity(new City(dto.getCityId(),null));
+	}
+
+}
